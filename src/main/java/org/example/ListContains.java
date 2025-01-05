d7e8c46990b310f7181b44bfa92d5adcc03cb87a
MuhammetOzturk1 <ozturk_muhammet08@hotmail.com>
2025-01-05 22:01:36 +0300

    ListContains Örneği Eklendi.

---
+++ src/main/java/org/example/ListContains.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ListContains {
+    public static void main(String[] args) {
+    String harf = "Ali";
+        List liste = new ArrayList();
+        liste.add("A");
+
+        System.out.println("İçerir" + liste.contains("A") );
+
+        }
+    }
